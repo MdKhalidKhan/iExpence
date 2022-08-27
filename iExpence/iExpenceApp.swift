1777b53dde7e9c78e8c7564086a8569c18a8953d
Raj <rajkhulna@outlook.com>
2022-08-27 12:38:26 +0600

    Initial Commit

---
+++ iExpence/iExpenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenceApp.swift
+//  iExpence
+//
+//  Created by Khalid Khan on 27/8/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
